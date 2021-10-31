871faee7bb7777fbd867dd733a0bed89a6ca2d23
Deepak <rverma13927@gmail.com>
2021-10-31 17:41:56 +0530

    added bean cors

--- src/main/java/com/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerceApplication.java
@@ -1,7 +1,9 @@
 package com.ecommerce;
 
+import com.ecommerce.config.SimpleCorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ECommerceApplication {
@@ -10,4 +12,9 @@ public class ECommerceApplication {
         SpringApplication.run(ECommerceApplication.class, args);
     }
 
+
+    @Bean
+    SimpleCorsFilter corsFilter() {
+        return new SimpleCorsFilter();
+    }
 }
