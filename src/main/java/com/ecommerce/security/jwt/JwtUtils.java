247abf28dc4d333b218dde17f775a26598442567
deepak <rverma13927gmail.com>
2021-11-18 11:29:59 +0530

    rename properties of jwt

--- src/main/java/com/ecommerce/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/security/jwt/JwtUtils.java
@@ -15,10 +15,10 @@ import java.util.Date;
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-    @Value("${bezkoder.app.jwtSecret}")
+    @Value("${jwtSecretKey}")
     private String jwtSecret;
 
-    @Value("${bezkoder.app.jwtExpirationMs}")
+    @Value("${jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
